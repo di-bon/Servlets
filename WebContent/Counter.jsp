5843465248afa5f7b9b1e3882cdff2c17d9fb802
FrancescoCapu <dibon.francesco03@gmail.com>
2022-03-24 10:08:54 +0100

    esercizio 1

---
+++ WebContent/Counter.jsp
@@ -0,0 +1,17 @@
+<%-- <%@ page 
+	language="java" 
+	contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"
+    import="demo.server.yeah.DemoServer"
+%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Ke supersballo</title>
+</head>
+<body>
+	<h1>Counter</h1>
+	<div>Visitors: <% int count = DemoServer.this.counter; System.out.println(count);%></div>
+</body>
+</html> --%>
--- src/demo/server/yeah/DemoServer.java
+++ src/demo/server/yeah/DemoServer.java
@@ -1,6 +1,8 @@
 package demo.server.yeah;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/DemoServer")
 public class DemoServer extends HttpServlet {
+	private static int counter = 0;
+	private ArrayList list;
 	private static final long serialVersionUID = 1L;
        
     /**
@@ -21,14 +25,42 @@ public class DemoServer extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+//    @Override
+    public void init() throws ServletException {
+    	super.init();
+    	counter = 0;
+    	list = new ArrayList();
+    }
+//    
+//    @Override
+//    public void destroy() {
+//    	counter--;
+//    	super.destroy();
+//    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("<h1>Supersballo Web App</h1><hr>");
+		counter++;
+		String shouldReset = request.getParameter("reset");
+		if (shouldReset != null) {
+			counter = 0;
+			response.sendRedirect("DemoServer");
+		}
+//		response.getWriter().append("" + counter);
+		response.getWriter().append("<h1>Su	persballo Web App</h1><hr>");
+		response.getWriter().append("Visitors: " + counter);
+		response.getWriter().append("<form action=\"DemoServer\" method=\"get\">");
+		response.getWriter().append("<input type=\"hidden\" name=\"reset\" value=\"yes\"/>");
+		response.getWriter().append("<input type=\"submit\" value=\"reset\">");
+		response.getWriter().append("</form>");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	public void reset() {
+		counter = 0;
 	}
 
 	/**
