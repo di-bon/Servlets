33707ae60901244ca427ca38e40f19139972b623
FrancescoCapu <dibon.francesco03@gmail.com>
2022-05-03 10:07:09 +0200

    implementation of cookies

--- src/cookies/form/Loginator.java
+++ src/cookies/form/Loginator.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.catalina.ha.session.SessionIDMessage;
+
 /**
  * Servlet implementation class Loginator
  */
@@ -41,22 +43,32 @@ public class Loginator extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
-		checkSession(request, response);
-		
 		String requestSource = request.getParameter("source");
-		
+
 		if (requestSource == null) {
 			System.out.println("The value of 'requestSource' is null");
 			RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/Login.jsp");
 			disp.forward(request, response);
 			return;
 		}
+		
+		checkSession(request, response, requestSource);
+		
+//		if (requestSource == null) {
+//			System.out.println("The value of 'requestSource' is null");
+//			RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/Login.jsp");
+//			disp.forward(request, response);
+//			return;
+//		}
 
 		System.out.println("Source: " + requestSource);	
 		if (requestSource.equals("login.jsp")) {
 			handleLogin(request, response);
 		} else if (requestSource.equals("logout.jsp")) {
 			handleLogout(request, response);
+		} else {
+			RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/Login.jsp");
+			disp.forward(request, response);
 		}
 	}
 
@@ -69,7 +81,7 @@ public class Loginator extends HttpServlet {
 			if (username.equals(STORED_USERNAME) && password.equals(STORED_PASSWORD)) {
 				System.out.println("username and password match");
 				HttpSession session = request.getSession();
-				session.setMaxInactiveInterval(60);
+				session.setMaxInactiveInterval(10);
 				session.setAttribute("username", username);
 				RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/Logout.jsp");
 				disp.forward(request, response);
@@ -82,11 +94,34 @@ public class Loginator extends HttpServlet {
 		}
 	}
 	
-	private void checkSession(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	private void checkSession(HttpServletRequest request, HttpServletResponse response, String requestSource) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-		if (session.isNew()) {
+		String pastUsername = (String) session.getAttribute("username");
+//		String pastPassword = (String) session.getAttribute("password");
+		
+//		if (pastUsername == null && !requestSource.equals("login.jsp")) {
+//			request.setAttribute("source", "login.jsp");
+//			RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/login.jsp");
+//			disp.forward(request, response);
+//			return;
+//		}
+		
+		
+		if (pastUsername == null) {
+//			RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/Login.jsp");
+//			disp.forward(request, response);
 			return;
 		}
+		
+		if (pastUsername.equals(STORED_USERNAME)) {
+			RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/Logout.jsp");
+			disp.forward(request, response);
+			return;
+		}
+		
+//		if (session.isNew()) {
+//			return;
+//		}
 		
 //		if (session.getAttribute(arg0)) {
 //			
@@ -94,6 +129,8 @@ public class Loginator extends HttpServlet {
 	}
 
 	private void handleLogout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.invalidate();
 		RequestDispatcher disp = request.getRequestDispatcher("/WEB-INF/Login.jsp");
 		disp.forward(request, response);
 		return;
